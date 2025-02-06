3f00eb4a680798a96148223e01534c76db9178a7
mjishu <128941980+Mjishu@users.noreply.github.com>
2025-02-05 21:40:05 -0500

    updated paths for images

--- src/main.ts
+++ src/main.ts
@@ -51,13 +51,12 @@ export class CreateTemplate {
     _createProjects(project: Project, image_left: boolean) {
         const createdProject = document.createElement('div');
         createdProject.className = 'project';
-        console.log('/images/' + project.image_src);
         createdProject.innerHTML = `
           ${
               image_left == true
                   ? `<div>
             <h4 class="project-name">${project.title}</h4>
-            <img class="project-image" src=${'public/images/' + project.image_src} alt="project-image"/>
+            <img class="project-image" src=${'/images/' + project.image_src} alt="project-image"/>
           </div>`
                   : ''
           }
@@ -70,7 +69,7 @@ export class CreateTemplate {
               image_left == false
                   ? `<div>
             <h4 class="project-name name-right">${project.title}</h4>
-            <img class="project-image" src=${'public/images/' + project.image_src} alt="project-image"/>
+            <img class="project-image" src=${'/images/' + project.image_src} alt="project-image"/>
           </div>`
                   : ''
           }
@@ -104,7 +103,7 @@ export class CreateTemplate {
         tagParent.className = 'tag-parent';
         for (const item of this.tags) {
             const tag = document.createElement('button');
-            const pathToIcon = `public/icons/${item.icon_src}`;
+            const pathToIcon = `/icons/${item.icon_src}`;
             tag.className = 'tag';
             tag.innerHTML = `
               ${item.iconFirst ? `<img class="tag-icon" src=${pathToIcon} alt="tag"/>` : ''}
