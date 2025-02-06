35c75ecec6344ede7734dda83b4065b9b5f7f8f9
mjishu <128941980+Mjishu@users.noreply.github.com>
2025-02-05 21:49:54 -0500

    changed index

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                entryFileNames: 'assets/main.js',
-            },
-        },
-    },
 });
